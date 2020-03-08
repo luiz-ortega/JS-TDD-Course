7cda773409026fc42195ee891994ea38dafc74ad
luiz-ortega <luizngust@hotmail.com>
2020-03-08 02:19:55 -0300

    Hooks

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,29 +1,29 @@
 describe('Main', () => {
   // roda uma vez, antes do bloco
-  before(function() {
+  before(() => {
     console.log('before');
   });
 
   // roda uma vez, depois do bloco
-  after(function() {
+  after(() => {
     console.log('after');
   });
 
   // roda todas as vezes, antes de CADA bloco
-  beforeEach(function() {
+  beforeEach(() => {
     console.log('beforeEach');
   });
 
   // roda todas as vezes, depois de CADA bloco
-  afterEach(function() {
+  afterEach(() => {
     console.log('afterEach');
   });
 
-  it('test 1', function() {
+  it('test 1', () => {
     console.log('Teste 1');
   });
 
-  it('test 2', function() {
+  it('test 2', () => {
     console.log('Teste 2');
   });
 });
