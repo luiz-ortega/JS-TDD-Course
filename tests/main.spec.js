b901c13fad8d792d58518a3a631f75087512edef
luiz-ortega <luizngust@hotmail.com>
2020-03-07 23:12:51 -0300

    01 - Test example

---
+++ tests/main.spec.js
@@ -0,0 +1,15 @@
+describe('Main', () => {
+  describe('Method A', () => {
+    context('Case 1', () => {
+      it('should happen blabla', () => {
+        // espera que aconteça
+        // Entra de dados / métodos sum(2,2)
+        // Espera retornar (4) => true | (3) => false => broken test
+      });
+    });
+
+    context('Case 2', () => {});
+  });
+
+  describe('Method B', () => {});
+});
