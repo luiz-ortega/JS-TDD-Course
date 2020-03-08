1e696339a4fe2eb82b96fde1bd7785fd62fa3605
luiz-ortega <luizngust@hotmail.com>
2020-03-08 02:16:23 -0300

    Hooks

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,15 +1,38 @@
 describe('Main', () => {
-  describe('Method A', () => {
-    context('Case 1', () => {
-      it('should happen blabla', () => {
-        // espera que aconteça
-        // Entra de dados / métodos sum(2,2)
-        // Espera retornar (4) => true | (3) => false => broken test
-      });
-    });
-
-    context('Case 2', () => {});
+  // roda uma vez, antes do bloco
+  before(function() {
+    console.log('before');
   });
 
-  describe('Method B', () => {});
+  // roda uma vez, depois do bloco
+  after(function() {
+    console.log('after');
+  });
+
+  // roda todas as vezes, antes de CADA bloco
+  beforeEach(function() {
+    console.log('beforeEach');
+  });
+
+  // roda todas as vezes, depois de CADA bloco
+  afterEach(function() {
+    console.log('afterEach');
+  });
+
+  it('test 1', function() {
+    console.log('Teste 1');
+  });
+
+  it('test 2', function() {
+    console.log('Teste 2');
+  });
 });
+
+// before
+// beforeEach
+// Teste 1
+// afterEach
+// beforeEach
+// Teste 2
+// afterEach
+// after
