e604d4926156827abdce9b8a8cdc3587e38c223a
luiz-ortega <luizngust@hotmail.com>
2020-03-09 22:10:49 -0300

    chai expects

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,39 +1,34 @@
+const { expect } = require('chai');
+
 describe('Main', () => {
   let arr;
-  // roda uma vez, antes do bloco
-  before(() => {});
-
-  // roda uma vez, depois do bloco
-  after(() => {});
 
   // roda todas as vezes, antes de CADA bloco
   beforeEach(() => {
     arr = [1, 2, 3];
   });
 
-  // roda todas as vezes, depois de CADA bloco
-  afterEach(() => {});
+  // testar tipos ou se existe (smoke test)
+  it('should be an array', () => {
+    expect(arr).to.be.a('array');
+  });
 
   it('should have a size of 4 when push another value to the array', () => {
     arr.push(4);
-    console.log(arr.length); // 4
+    expect(arr).to.have.length(4);
   });
 
   it('should have a size of 2 when pop a value from the array', () => {
     arr.pop();
-    console.log(arr.length); // 2
+    expect(arr).to.not.include(3);
+  });
+
+  it('should return true when pop3 from the array', () => {
+    expect(arr.pop() === 3).to.be.true;
   });
 
   it('should remove the value 3 when use pop in the array', () => {
-    console.log(arr.pop() === 3); // true
+    arr.pop();
+    expect(arr).to.have.lengthOf(2);
   });
 });
-
-// before
-// beforeEach
-// Teste 1
-// afterEach
-// beforeEach
-// Teste 2
-// afterEach
-// after
